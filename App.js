c57bb8f95f34e199a9e2873a70e576aa7bc44893
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2021-01-01 19:20:58 -0300

    Tela Home

--- App.js
+++ App.js
@@ -3,19 +3,31 @@ import { Text, View, Button, Alert } from 'react-native';
 import {css} from './assets/css/Css';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Home from './views/Home';
-import Login from './views/Login';
+import {Home,Login,Rastreio} from './views';
 
 export default function App() {
 
   const Stack = createStackNavigator();
 
   return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Login" component={Login} />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+    
+    <Stack.Navigator>
+      <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+              title:"CAIRITONLOG",
+              headerStyle:{backgroundColor:"#F58634"},
+              headerTintColor:'#333',
+              headerTitleStyle:{fontWeight:'bold', alignSelf:'center'}
+          }}
+      />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Rastreio" component={Rastreio} />
+      {/*<Stack.Screen name="AreaRestrita" component={AreaRestrita} />*/}
+    </Stack.Navigator>
+    
+  </NavigationContainer>
   );
 }

5378c001bbecd2cff1c28ee7780f7def1e8b6037
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2021-01-02 19:58:13 -0300

    Adicionando biometria no login

--- views/Login.js
+++ views/Login.js
@@ -2,66 +2,105 @@ import React, {useState, useEffect} from 'react';
 import { KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, Image, Text, View} from 'react-native';
 import { css } from "../assets/css/Css";
 import AsyncStorage from '@react-native-community/async-storage';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 
-export default function Login({navigation})
+export default function Login({navigation}){
+
+    const [display, setDisplay]=useState('none');
+    const [user, setUser]=useState(null);
+    const [password, setPassword]=useState(null);
+    const [login, setLogin]=useState(null);
+    useEffect(()=>{
+        verifyLogin();
+    },[]);
+    
+    useEffect(()=>{
+        if(login === true){
+            biometric();
+        }
+    },[login]);
+
+
+    //Verifica se o usuário já possui algum login
+async function verifyLogin()
 {
+    let response=await AsyncStorage.getItem('userData');
+    let json=await JSON.parse(response);
+    if(json !== null){
+        setUser(json.name);
+        setPassword(json.password);
+        setLogin(true);
+    }
+}
 
-  const [display, setDisplay] = useState('none');
-  const [user, setUser]=useState(null);
-  const [password, setPassword]=useState(null);
-  const [login, setLogin]=useState(null);
+//Biometria
+async function biometric()
+{
+    let compatible= await LocalAuthentication.hasHardwareAsync();
+    if(compatible){
+        let biometricRecords = await LocalAuthentication.isEnrolledAsync();
+        if(!biometricRecords){
+            alert('Biometria não  está cadastrada');
+        }else{
+            let result=await  LocalAuthentication.authenticateAsync();
+            if(result.success){
+                sendForm();
+            }else{
+                setUser(null);
+                setPassword(null);
+            }
+        }
+    }
+}
 
-  //Envio do formulário de login
-  async function sendForm()
-  {
-      let response=await fetch('http://192.168.15.9:3000/login',{
-          method: 'POST',
-          headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-              name: user,
-              password: password
-          })
-      });
-      let json=await response.json();
-      if(json === 'error'){
-          setDisplay('flex');
-          setTimeout(()=>{
-              setDisplay('none');
-          },5000);
-          await AsyncStorage.clear();
-      }else{
+
+
+ //Envio do formulário de login
+async function sendForm()
+{
+    let response=await fetch('http://192.168.15.9:3000/login',{
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: user,
+            password: password
+        })
+    });
+    let json=await response.json();
+    if(json === 'error'){
+        setDisplay('flex');
+        setTimeout(()=>{
+            setDisplay('none');
+        },5000);
+        await AsyncStorage.clear();
+    }else{
         await AsyncStorage.setItem('userData', JSON.stringify(json));
         navigation.navigate('AreaRestrita');
-
-      }
-  }
-
+    }
+}
 
   
-    return(
-      <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style={[css.container, css.darkbg]}>
-      <View  style={css.login__logomarca}>
-      <Image source={require('../assets/img/icon.png')} /> 
-       
-      </View>
+  return(
+    <KeyboardAvoidingView style={[css.container, css.darkbg]}>
+        <View style={css.login__logomarca}>
+            <Image source={require('../assets/img/icon.png')} />
+        </View>
 
-      <View>
-      <Text style={css.login__msg(display)}>Usuário ou senha Inválidos!</Text>
-      </View>
+        <View>
+            <Text style={css.login__msg(display)}>Usuário ou senha inválidos!</Text>
+        </View>
 
-      <View style={css.login__form}>
-                <TextInput style={css.login__input} placeholder='Usuário:' onChangeText={text=>setUser(text)} />
-                <TextInput style={css.login__input} placeholder='Senha:' onChangeText={text=>setPassword(text)} secureTextEntry={true} />
-                <TouchableOpacity style={css.login__button} onPress={()=>sendForm()}>
-                    <Text style={css.login__buttonText}>Entrar</Text>
-                </TouchableOpacity>
-            </View>
-
-
-     </KeyboardAvoidingView>
-    );
+        <View style={css.login__form}>
+            <TextInput style={css.login__input} placeholder='Usuário:' onChangeText={text=>setUser(text)} />
+            <TextInput style={css.login__input} placeholder='Senha:' onChangeText={text=>setPassword(text)} secureTextEntry={true} />
+            <TouchableOpacity style={css.login__button} onPress={()=>sendForm()}>
+                <Text style={css.login__buttonText}>Entrar</Text>
+            </TouchableOpacity>
+        </View>
+    </KeyboardAvoidingView>
+);
 }
